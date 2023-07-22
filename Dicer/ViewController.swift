9a789428185007c22342a0a79d355cc44dc3e57a
Atharva <79651393+Atharva-1516@users.noreply.github.com>
2023-07-21 20:27:23 -0700

    Initial Commit

---
+++ Dicer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicer
+//
+//  Created by Atharva suryawanshi on 2023-07-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DicerUITests/DicerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DicerUITestsLaunchTests.swift
+//  DicerUITests
+//
+//  Created by Atharva suryawanshi on 2023-07-21.
+//
+
+import XCTest
+
+final class DicerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
